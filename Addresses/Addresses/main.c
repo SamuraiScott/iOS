7abba707f732d479884a3e1f07b329d1c12ae5d2
Scott <sam_urai_Scott@hotmail.com>
2014-08-13 17:13:05 -0400

    Chapter 10

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -11,8 +11,15 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+    int *addressOfI = &i;
+    printf("i stores its value at %p\n", addressOfI);
+    printf("This funtion starts at %p\n", main);
+    printf("the int stored at addressOfI is %d\n", *addressOfI);
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(i));
+    printf("A pointer is %zu bytes\n", sizeof(addressOfI));
     return 0;
 }
 
