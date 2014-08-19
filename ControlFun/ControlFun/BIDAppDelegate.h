1ba3371203c6bdc3a78c7be7dbe2d8ed0ce0e31e
Scott <sam_urai_Scott@hotmail.com>
2014-08-19 16:44:33 -0400

    first commit of Control Fun tutorial from beginning iOS 7 Development

---
+++ ControlFun/ControlFun/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  ControlFun
+//
+//  Created by Scott on 2014-08-19.
+//  Copyright (c) 2014 ScottMaile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ControlFun/ControlFun/BIDViewController.h
@@ -0,0 +1,15 @@
+//
+//  BIDViewController.h
+//  ControlFun
+//
+//  Created by Scott on 2014-08-19.
+//  Copyright (c) 2014 ScottMaile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+- (IBAction)textFieldDoneEditing:(id)sender;
+
+@end
