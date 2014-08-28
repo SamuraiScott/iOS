72801071f9e9179cf705a197183a33c5ff9255c2
Scott <sam_urai_Scott@hotmail.com>
2014-08-28 15:50:36 -0400

    Chapter 7 complete (Tab Bars And Pickers)

--- Pickers/Pickers/BIDCustomPickerViewController.h
+++ Pickers/Pickers/BIDCustomPickerViewController.h
@@ -2,12 +2,13 @@
 //  BIDCustomPickerViewController.h
 //  Pickers
 //
-//  Created by Scott on 2014-08-25.
+//  Created by Scott on 2014-08-27.
 //  Copyright (c) 2014 ScottMaile. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface BIDCustomPickerViewController : UIViewController
+<UIPickerViewDataSource, UIPickerViewDelegate>
 
 @end
